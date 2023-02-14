d55b8e1fd58b75b058c05714be172dd44785f49f
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2023-02-14 12:21:32 +1000

    .config/nvim: add haskell-tools plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,10 +1,17 @@
 return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'   -- package manager
-
-    use 'neovim/nvim-lspconfig'    -- nvim LSP configs
-    use 'hrsh7th/nvim-cmp'         -- autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp'     -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip'         -- snippets plugin
+    use 'wbthomason/packer.nvim'             -- package manager
+    use 'neovim/nvim-lspconfig'              -- nvim LSP configs
+    use 'hrsh7th/nvim-cmp'                   -- autocompletion plugin
+    use 'hrsh7th/cmp-nvim-lsp'               -- LSP source for nvim-cmp
+    use 'saadparwaiz1/cmp_luasnip'           -- snippets source for nvim-cmp
+    use 'L3MON4D3/LuaSnip'                   -- snippets plugin
+    use {                                    -- haskell-tools plugin
+        'mrcjkb/haskell-tools.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim', -- optional
+        },
+        branch = '1.x.x', -- recommended
+    }
 end)
 
