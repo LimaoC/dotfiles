975c2bcb57253bc4cb70de0a9fa3a27c970b45a6
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2023-10-20 20:32:57 +1000

    oh-my-zsh: add top5 alias for printing top 5 most used commands

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -1,5 +1,5 @@
 # for managing my dotfiles repository, I use `config` instead of `git`
-# see https://www.atlassian.com/git/tutorials/dotfiles
+# REF: https://www.atlassian.com/git/tutorials/dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 alias ,="cd .."
@@ -11,4 +11,8 @@ alias python=python3
 # Add an "alert" alias for long running commands, use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Top 5 most frequently used comments
+# REF: https://old.reddit.com/r/archlinux/comments/t4ohgq/what_are_your_top_5_most_used_shell_commands/
+alias top5='print -l ${(o)history%% *} | uniq -c | sort -nr | head -n 5'
 
