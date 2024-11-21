e27c56c8ae545ba42d67925f27e925d5adc9d243
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2024-11-21 21:32:11 +1100

    zsh: remove python alias

--- zsh/.oh-my-zsh-custom/aliases.zsh
+++ zsh/.oh-my-zsh-custom/aliases.zsh
@@ -2,8 +2,6 @@ alias ,="cd .."
 alias ,,="cd ../.."
 alias ,,,="cd ../../.."
 
-alias python=python3
-
 # Add an "alert" alias for long running commands, use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
